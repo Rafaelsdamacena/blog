58a3eec9b17a14ea9a3f9579f0e7c337c1667154
Rafael <rafaelsdamacena@gmail.com>
2024-01-25 21:32:21 -0300

    template header

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
 <?php
-    include_once("helpers/url.php");
+    include_once("templates/header.php");
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog Rafael</title>
-
-    <!--style-->
-    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,700;1,300;1,700&display=swap" rel="stylesheet">
-
-</head>
-<body>
-    <h1> Olá Mundo! </h1>
+    <h1>Olá Mundo!! </h1>
 </body>
 </html>
