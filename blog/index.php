c243bc5994167df7bce0f89a5b17e46c5cc8d789
Rafael <rafaelsdamacena@gmail.com>
2024-01-31 21:46:30 -0300

    directory change

---
+++ blog/index.php
@@ -0,0 +1,34 @@
+<?php
+    include_once("templates/header.php");
+?>
+
+    <main>
+        <div id="title-container">
+            <h1>TÍTULO</h1>
+            <p> Subtítulo </p>
+        </div>
+
+        <div id="post-container">
+            <?php foreach($posts as $post): ?>
+                <div class="post-box">
+                    <img src="<?= $BASE_URL ?>/img/<?= $post['img']?>" alt="<?= $post['title'] ?>">
+                    <h2 class="post-title">
+                        <a href="<?= $BASE_URL ?>post.php?id=<?= $post['id']?>"><?= $post['title'] ?>
+                    </h2>
+                    <p class="post-description"><?= $post['description']?></p>
+                    <div class="tags-container"> 
+                        <?php foreach($posts['tags'] as $tag): ?>
+                            <a href="#"> <?= $tag ?> </a>
+                        <?php endforeach; ?>
+
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
+    </main>
+    
+
+<?php
+    include_once("templates/footer.php");
+?>
---
+++ blog/templates/header.php
@@ -0,0 +1,34 @@
+<?php
+    include_once("helpers/url.php");
+    include_once("data/posts.php");
+    include_once("data/categories.php");
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rafael Santos</title>
+
+    <!--style-->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,700;1,300;1,700&display=swap" rel="stylesheet">
+
+</head>
+    <body>
+        <header>
+            <a href="<?= $BASE_URL ?>" id="logo">
+                <img src="<?= $BASE_URL ?>/img/eu.png" alt="Rafael Santos">
+            </a>
+            <nav>
+                <ul id="navbar">
+                    <li><a href="<?= $BASE_URL ?>" class="nav-link"> HOME </a></li>
+                    <li><a href="<?= $BASE_URL ?>" class="nav-link"> PROJECTS </a></li>
+                    <li><a href="<?= $BASE_URL ?>" class="nav-link"> CONTACT </a></li>
+                </ul>
+            </nav>
+        </header>
+    
