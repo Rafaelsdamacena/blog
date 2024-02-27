52ff7fffccdef35c3f8b2e3614408112f90bf034
Rafael <rafaelsdamacena@gmail.com>
2024-02-26 21:47:34 -0300

    added text to array in posts.php

--- blog/data/posts.php
+++ blog/data/posts.php
@@ -6,14 +6,16 @@
       'title' => 'Um pouco de mim',
       'description' => 'Aqui vou me apresentar e falar um pouco sobre mim',
       'tags' => ['Trajetória', 'Habilidades', 'Interesses'],
-      'img' => 'rafael-capa.jpeg'
+      'img' => 'rafael-capa.jpeg',
+      'text' => 'texto1'
     ],
     [
       'id' => 2,
       'title' => 'Um pouco da minha jornada',
       'description' => 'Aqui vou apresentar um pouco da minha trajetória profissional',
       'tags' => ['Sobre', 'Habilidades', 'Interesses'],
-      'img' => 'trajetoria.jpg'
+      'img' => 'trajetoria.jpg',
+      'text' => 'texto2'
     ],
     [
       'id' => 3,
@@ -21,12 +23,14 @@
       'description' => 'Aqui vou falar um pouco sobre minhas skills',
       'tags' => ['Sobre', 'Trajetória', 'Interesses'],
       'img' => 'habilidade.jpg',
+      'text' => 'texto3'
     ],
     [
       'id' => 4,
       'title' => 'Um pouco dos meus interesses',
       'description' => 'Aqui vou falar um pouco sobre o que eu gosto de fazer',
       'tags' => ['Sobre', 'Trajetória', 'Habilidades'],
-      'img' => 'interesses.jpg'
+      'img' => 'interesses.jpg',
+      'text' => 'texto4'
     ],
   ];
--- blog/post.php
+++ blog/post.php
@@ -20,11 +20,8 @@
             <div class="img-container">
                 <img src="<?= $BASE_URL ?>/img/<?= $currentPost['img'] ?>" alt="<?= $currentPost['title'] ?>">
             </div>
-            <p class="post-content">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium deleniti modi quam rem, blanditiis qui inventore quos, aut esse laudantium quisquam beatae atque laboriosam consequuntur voluptatibus nobis at molestiae assumenda.
-            Omnis odit cumque autem explicabo, quae veritatis molestias minus, unde magni itaque molestiae magnam illo harum soluta ut repudiandae culpa sit temporibus? Eveniet placeat officiis magnam ratione cumque? Sit, atque.
-            Voluptatem corrupti ipsum quia laboriosam inventore ducimus ex aspernatur, voluptatum dolorem soluta facere minima voluptates, reiciendis adipisci earum! Eos minus veritatis vero officia optio dicta quam id quasi sapiente quidem.
-            Est suscipit esse, molestiae adipisci repellendus recusandae. Quo blanditiis repellendus nobis! Aliquid cum error, sed excepturi beatae perspiciatis illo doloremque doloribus itaque? Dicta quam maiores non, explicabo vero odit doloremque.
-            Excepturi placeat, maxime error dicta impedit assumenda pariatur provident natus nulla rem dolorem, nostrum molestiae praesentium quis tempore nemo incidunt. Tempora commodi reiciendis quae itaque asperiores repudiandae veniam aperiam odit?
+            <p class="post-content">
+                <?=$currentPost['text'] ?>
             </p>
         </div>
 
